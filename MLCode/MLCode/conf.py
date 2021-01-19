1f444a5298e1b4a297e9166470e3a202dc2e8c4a
FeLusiani <felusiani@gmail.com>
2021-01-19 19:47:54 +0100

    Changed prj root path

--- MLCode/MLCode/conf.py
+++ MLCode/MLCode/conf.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-path_prj        = Path('/home/ubuntu/MachineLeaningProject/ML_Project')
+path_prj        = Path(__file__).parents[2]
 path_data       = path_prj  / Path('./data')
 TR_CUP_csv      = path_data / Path('./ML_CUP/ML-CUP20-TR.csv')
 TS_CUP_csv      = path_data / Path('./ML_CUP//ML-CUP20-TS.csv')
